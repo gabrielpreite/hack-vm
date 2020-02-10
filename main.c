3e4ef5b7d3430eadcd172979307fd61c32c9b4dd
Gabriel Preite <gabriel@gabriel-Laptop.lan>
2020-02-10 11:26:00 +0100

    command tokenizer done

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <ctype.h>
 
-typedef struct comando{
-    char a[50], b[50];
-    int c;
-    struct comando *prev;
+typedef struct command{
+    char *a, *b, *c;
+    struct command *prev;
 }com;
 
-typedef struct comando* ptr_stack;
+typedef struct command* ptr_stack;
 ptr_stack it = NULL;
 
 int main(int argc, char **argv){
@@ -31,32 +31,69 @@ int main(int argc, char **argv){
     while(fgets(riga, 100, fin)){
         //printf("a:%d\n", riga[0]);
         if(riga[0] != '\r' && riga[0] != '/'){
-            //printf("b:%d\n", riga[0]);
+            /*//printf("b:%d\n", riga[0]);
             int i=0;
             char a[] = "";
             char b[] = "";
             char c[] = "";
-            for(; i<100 && !isspace(a[i]); i++){//legge prima parte
+            for(; i<100 && !isspace(riga[i]); i++){//legge prima parte
                 a[i] = riga[i];
-                printf("a:%c\n", riga[i]);
+                //printf("a:%c\n", riga[i]);
             }
 
             int offset = i;
             for(; i<100 && !isspace(riga[i]); i++){//legge seconda parte
                 b[(i-offset)] = riga[i];
-                printf("b:%c\n", riga[i]);
+                //printf("b:%c\n", riga[i]);
             }
             
             offset = i;
             for(; i<100 && !isspace(riga[i]); i++){//legge terza parte
                 c[(i-offset)] = riga[i];
-                printf("c:%c\n", riga[i]);
+                //printf("c:%c\n", riga[i]);
+            }*/
+            char *a = (char*)malloc(50);
+            char *b = (char*)malloc(50);
+            char *c = (char*)malloc(50);
+            char *token;
+            char s[] = " ";
+
+            token = strtok(riga, s);//load a
+            strcpy(a, token);
+        
+            token = strtok(NULL, s);
+            if(token != NULL){
+                strcpy(b, token);//load b
+
+                token = strtok(NULL, s);
+                if(token != NULL)
+                    strcpy(c, token);//load c
             }
-            //printf("%s,%s,%d", a, b, c);
-            ptr_stack x = {a, b, atoi(c), it};
-            //printf("%s,%s,%d", it->a, it->b, it->c);
-            it = x;
+
+            printf("a:%s, b:%s, c:%s\n", a, b, c);
+
+            if(!strcmp(a, "push")){
+                struct command x;
+                x.a = (char*)malloc(50);
+                x.b = (char*)malloc(50);
+                x.c = (char*)malloc(50);
+                x.prev = it;
+                strcpy(x.a, a);
+                strcpy(x.b, b);
+                strcpy(x.c, c);
+                //printf("a:%s, b:%s, c:%s", a, b, c);
+                //printf("%s,%s,%d", it->a, it->b, it->c);
+                it = &x;
+            }
+            
         }
     }
+    while(it != NULL){
+        printf("%s", it->c);
+        it = it->prev;
+        getchar();
+    }
+    fclose(fin);
+    fclose(fout);
     return 0;
 }
